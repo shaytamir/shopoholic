0272a572518a8cac1581dcb29976590db0ce686d
shay <shay.lom.tamir@gmail.com>
2021-05-03 12:19:03 +0300

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+import AppRouter from "./components/appRouter";
+
+export const appContext = React.createContext();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <appContext.Provider value={{}}>
+        <header>
+          {/* messages */}
+          <ToastContainer />
+        </header>
+        <div className="demo_header"></div>
+
+        <main className="main">
+          <AppRouter />
+        </main>
+        <footer></footer>
+      </appContext.Provider>
     </div>
   );
 }
