0ad25bc4d48e977f71d421806c5b530696cbb804
shay <shay.lom.tamir@gmail.com>
2021-05-08 23:27:00 +0300

    production fix

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join("client/build")));
 
   // app.use(express.static("build"));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
   // var distDir = __dirname + "/dist/";
